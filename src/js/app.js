1718eafb4c358344af4f717a89cbbdd0d3cda68c
blongden73 <hello@benlongdendesign.co.uk>
2020-02-17 12:56:59 +0000

    added sticky video

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,32 @@
-// js code here
+function sticky(){
+  var tracker = document.querySelector('.sticky-tracker');
+  var stickyContent = document.querySelector('.sticky-content');
+  document.addEventListener('scroll', function(){
+    var bottom = tracker.getBoundingClientRect().bottom;
+    console.log(bottom);
+    if(bottom <= 0) {
+      stickyContent.classList.add('sticky-video')
+    }else {
+      stickyContent.classList.remove('sticky-video')
+    }
+  });
+}
+
+function timeStamps(){
+  var timeStamp = document.querySelectorAll('.time-stamp');
+  for(var i = 0; i < timeStamp.length; i++) {
+    console.log(timeStamp[i].innerHTML);
+    var rawDate = timeStamp[i].innerHTML;
+    var format = moment().format(rawDate);
+    var now = moment();
+    var diff = moment().diff(format, 'days');
+    timeStamp[i].innerHTML = diff + ' days ago';
+    console.log(diff);
+  }
+}
+
+function init(){
+  sticky();
+  timeStamps();
+}
+init();
